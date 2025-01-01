91bd869512adfe1f93a41a48c53af0d72c1aba60
olawale7040 <olawale7040@gmail.com>
2025-01-01 19:22:42 +0000

    feat: Refactor SymbolCard to enhance component granularity

---
+++ frontend/src/components/SymbolCard/src/TrendArrow.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import UpArrow from '@/assets/up.png';
+import DownArrow from '@/assets/down.png';
+
+export const getTrendIcon = (trend: 'UP' | 'DOWN' | null) => {
+  switch (trend) {
+    case 'UP':
+      return <img src={UpArrow} className="symbolCard__trend" alt="Up Arrow" />;
+    case 'DOWN':
+      return <img src={DownArrow} className="symbolCard__trend" alt="Down Arrow" />;
+    default:
+      return null;
+  }
+};
+
+type TrendArrowProps = {
+  trend: 'UP' | 'DOWN' | null;
+};
+
+const TrendArrow: FC<TrendArrowProps> = ({ trend }) => {
+  return getTrendIcon(trend);
+};
+
+export default TrendArrow;
